d7f3c1bac264df3540aa6caa99d794c2aae412b4
Love Dahlberg <love.dahlberg@hotmail.com>
2023-04-19 19:10:18 +0200

    Add Serializable dict library

--- Assets/Scripts/Control/GameController.cs
+++ Assets/Scripts/Control/GameController.cs
@@ -1,48 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+using UnityEditor;
 namespace Controller
 {
     public class GameController : MonoBehaviour
     {
-        [SerializeField]
-        private List<Stage> topLevelStages;
+       
+        public StageStorage topLevelStages;
         private Stage currentStage;
         private Names StartStage;
 
         void Start()
         {
-            currentStage = topLevelStages.Find(x => x.GetName() == (int)StartStage);
-            if (currentStage is null)
-            {
-                Debug.Log(new System.Exception("GameController: Start " + StartStage + " stage not found"));
-                return;
-            }
+            //currentStage = topLevelStages.Find(x => x.GetName() == (int)StartStage);
+            //if (currentStage is null)
+            //{
+            //    Debug.Log(new System.Exception("GameController: Start " + StartStage + " stage not found"));
+            //    return;
+            //}
 
-            if (currentStage is not null)
-            {
-                currentStage.StartStage(topLevelStages);
-            }
+            //if (currentStage is not null)
+            //{
+            //    currentStage.StartStage(topLevelStages);
+            //}
         }
 
 
         private void Update()
         {
-            var newStage = currentStage?.Continue();
-            if(newStage is not null)
-            {
-                currentStage = newStage;
-                currentStage.StartStage(topLevelStages);
-                return;
-            }
+            //var newStage = currentStage?.Continue();
+            //if(newStage is not null)
+            //{
+            //    currentStage = newStage;
+            //    currentStage.StartStage(topLevelStages);
+            //    return;
+            //}
         }
     }
 
-    enum Names
+
+    public enum Names
     {
         INTERVIEW,
         MATCH,
         DATE
     }
 }
+
+[Serializable]
+public class StageStorage : SerializableDictionary<Controller.Names, Stage> { }
